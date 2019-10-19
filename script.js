c22b1dbfc6c15145f0733bf7409cef476c1eee0b
7esoterik7 <7esoterik7@gmail.com>
2019-10-19 18:53:19 +0400

    Prepared files

---
+++ script.js
@@ -0,0 +1,16 @@
+﻿var money = prompt('Ваш бюджет на месяц?', '');
+var time = prompt('Введите дату в формате YYYY-MM-DD', '');
+var obligatoryExpenses = prompt('Введите обязательную статью расходов в этом месяце', '');
+var priceOobligatoryExpenses = prompt('Во сколько обойдется?', '');
+var exp = {};
+exp[obligatoryExpenses] = priceOobligatoryExpenses;
+
+var appData = {budget: money,
+               timeData: time,
+               expenses: exp,
+               optionalExpenses: {},
+               income: [],
+               savings: false
+               };
+
+alert('На один день' + appData.budget / 30);
