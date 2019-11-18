018ee57b32b48854ad795455fe5e1130d5a92a00
7esoterik7 <7esoterik7@gmail.com>
2019-11-18 21:26:32 +0400

    Make timer

--- Yoga/js/script.js
+++ Yoga/js/script.js
@@ -33,4 +33,73 @@ window.addEventListener('DOMContentLoaded', function() {
 		}
 	});
 	
+	//Timer
+	
+	var deadline = '2019-11-19';
+	
+	function getTimeRemaining(endtime) {
+		var t = Date.parse(endtime) - Date.parse(new Date());
+		var seconds = Math.floor((t / 1000) % 60);
+		var minutes = Math.floor((t / 1000 / 60) % 60);
+		var hours = Math.floor((t / 1000 / 60 / 60));
+		
+		return {
+			'total': t,
+			'hours': hours,
+			'minutes': minutes,
+			'seconds': seconds
+		}
+	}
+	
+	function setClock(id, endtime) {
+		var timer = document.getElementById(id);
+		var hours = timer.querySelector('.hours');
+		var minutes = timer.querySelector('.minutes');
+		var seconds = timer.querySelector('.seconds');
+		var timeInterval = setInterval(updateClock, 1000);
+		
+		function updateClock() {
+			var t = getTimeRemaining(endtime);
+			hours.textContent = String(t.hours).length === 2 ? t.hours : '0' + t.hours;
+			minutes.textContent = String(t.minutes).length === 2 ? t.minutes : '0' + t.minutes;
+			seconds.textContent = String(t.seconds).length === 2 ? t.seconds : '0' + t.seconds;
+			
+			if (t.total <= 0) {
+				clearInterval(timeInterval);
+			}
+		}
+	}
+	
+	setClock('timer', deadline);
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
